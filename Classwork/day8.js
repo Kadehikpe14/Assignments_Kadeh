09eb25ab9af8824be1e7bacc8cf6df4161a2bd53
Kadeh Ikpe <40577808+Kadehikpe14@users.noreply.github.com>
2022-04-25 18:42:49 -0400

    Delete Classwork directory

--- Classwork/day8.js
+++
@@ -1,7 +0,0 @@
-function changeParagraph() {
-	document.getElementById("demo").innerHTML="Hello JavaScript!";
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-	changeParagraph();
-});
